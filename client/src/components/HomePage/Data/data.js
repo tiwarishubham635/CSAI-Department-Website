e39f948a1fc4083b5b6b3ee4a7a87e4b53630456
Ritesh Kumar <riteshkumarstudy@gmail.com>
2020-12-24 16:28:49 +0530

    Homepage Edits

--- client/src/components/HomePage/Data/data.js
+++ client/src/components/HomePage/Data/data.js
@@ -2,11 +2,11 @@ export default [
     {
         title: "Created By",
         content: [
-            "Pranay Kothari",
             "Yash Sethia",
             "Shubham",
-            "Ashwani Tanwar",
-            "Ritesh Kumar"
+            "Ritesh Kumar",
+            "Pranay Kothari",
+            "Ashwani Tanwar"
                 ]
             }
 ];
